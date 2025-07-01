81bf8e008884c02b7a3b9b18d12ac6d6a2c7fa35
Ayesha Naveed <hr34772@gmail.com>
2025-07-02 03:21:27 +0500

    change

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,24 +3,24 @@ import React from "react";
 export default function Login() {
   return (
     <div className="flex flex-col justify-around h-screen bg-slate-950 object-cover ">
-      <div className="flex w-full h-screen">
+      <div className=" flex  w-1/2 h-screen">
         <img
-          className="flex w-full object-cover opacity-50"
+          className="w-full h-full object-cover opacity-50"
           src="image 6.png"
           alt="image not found"
         />
 
-        <div className="flex gap-8 flex-col justify-center items-center w-full h-full h-screen">
+        <div className="flex gap-8 w-1/2 flex-col justify-center items-center w-full h-full h-screen ">
           <img src="Frame 1171274783.png" alt="image not found" />
           <h2 className="text-white font-onset font-semibold text-2xl  ">
             Login
           </h2>
-          <p className="text-gray-400 font-onset font-normal text-base">
+          <p className="text-gray-400 font-onset font-normal text-base text-center">
             Login to oversee all restaurants, manage users and control platforms
             wide setting with full access
           </p>
           <div className="mb-5">
-            <form action="login">
+            <form action="login" className="flex flex-col justify-center ">
               <label
                 className="text-white font-normal font-onset"
                 htmlFor="email"
@@ -28,7 +28,7 @@ export default function Login() {
                 Email
               </label>
               <input
-                className="bg-cyan-950 text-white text-sm block mb-2 font-onset m-4 p-4 rounded-md "
+                className="bg-cyan-950 text-white text-sm block mb-2 font-onset m-4 p-2.5 rounded-md w-full "
                 type="text"
                 name="Email"
                 id="email"
@@ -41,12 +41,12 @@ export default function Login() {
                 Password
               </label>
               <input
-                className="bg-cyan-950 text-white text-sm block mb-2 font-onset m-4 p-4 rounded-md "
+                className="bg-cyan-950 text-white text-sm block mb-2 font-onset m-4 p-2.5 rounded-md w-full"
                 type="password"
                 name="password"
                 placeholder="Password"
               />
-              <button className="bg-rose-400 text-white m-4 p-4 rounded-md">
+              <button className="bg-rose-400 text-white m-4 p-2.5 rounded-md w-full">
                 Login
               </button>
             </form>
