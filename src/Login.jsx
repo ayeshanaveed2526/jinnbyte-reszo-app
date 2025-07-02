8586bb8ac450282c08ff49198e9270103697092b
Ayesha Naveed <hr34772@gmail.com>
2025-07-02 17:46:06 +0500

    login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,13 @@
-import React from "react";
+import React from 'react'
 
 export default function Login() {
   return (
-    <div className="flex flex-col justify-around h-screen bg-slate-950 object-cover ">
-      <div className=" flex  w-1/2 h-screen">
-        <img
-          className="w-full h-full object-cover opacity-50"
-          src="image 6.png"
-          alt="image not found"
-        />  
-
-        <div className="flex gap-8 w-1/2 flex-col justify-center items-center w-full h-full h-screen ">
-          <img src="Frame 1171274783.png" alt="image not found" />
+    <div className='bg-slate-950 flex flex-row justify-center items-center h-screen'>
+      <div className='w-1/2 h-screen flex justify-center items-center'>
+        <img src="image 6.png" alt="image not found" className='w-full h-full object-cover opacity-50'/>
+      </div>
+      <div className='w-1/2 h-screen flex flex-col gap-8 justify-center items-center'>
+        <img src="Frame 1171274783.png" alt="image not found" />
           <h2 className="text-white font-onset font-semibold text-2xl  ">
             Login
           </h2>
@@ -20,20 +16,23 @@ export default function Login() {
             wide setting with full access
           </p>
           <div className="mb-5">
-            <form action="login" className="flex flex-col justify-center ">
+            <form action="login" className="flex flex-col items-start justify-center ">
               <label
                 className="text-white font-normal font-onset"
                 htmlFor="email"
               >
                 Email
               </label>
-              <input
-                className="bg-[#1B2D47] text-white text-sm block mb-2 font-onset m-4 p-2.5 rounded-md w-full "
+              
+                <input
+                className="bg-[#1B2D47] text-white text-sm block mb-2 font-onset m-4 px-4 py-3 rounded-md w-full  "
                 type="text"
                 name="Email"
                 id="email"
                 placeholder="Email address"
+                required
               />
+            
               <label
                 className="text-white font-normal font-onset"
                 htmlFor="password"
@@ -41,18 +40,19 @@ export default function Login() {
                 Password
               </label>
               <input
-                className="bg-[#1B2D47] text-white text-sm block mb-2 font-onset m-4 p-2.5 rounded-md w-full"
+                className="bg-[#1B2D47] text-white text-sm block mb-2 font-onset m-4 px-4 py-3 pr-10 rounded-md w-full"
                 type="password"
                 name="password"
                 placeholder="Password"
+                required
               />
               <button className="bg-[#B04759] text-white m-3 p-2.5 rounded-md w-full">
                 Login
-              </button>
+ </button>
             </form>
           </div>
-       </div>
+
       </div>
     </div>
-  );
+  )
 }
