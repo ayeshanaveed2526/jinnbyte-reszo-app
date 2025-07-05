4dc72b71c23dfcbcd9c7bb803eb82b9b0e757903
Ayesha Naveed <hr34772@gmail.com>
2025-07-05 19:02:35 +0500

    customer management

--- src/EmptyTable.jsx
+++ src/EmptyTable.jsx
@@ -3,18 +3,28 @@ import React from 'react'
 export default function EmptyTable() {
   return (
     <div>
-      <div className="bg-[#131E2E] border border-gray-700 rounded-xl p-5 ">
-<div className=' bg-[#1B2D47] border border-gray-600 rounded-xl px-4 py-2 text-gray-400 flex flex-row '>
+      <div className="bg-[#131E2E] border border-gray-700 rounded-xl px-5 py-5  w-[1092px] h-[406.1px] ">
+<div className=' bg-[#1B2D47] border border-gray-600 rounded-xl px-4 py-2 text-gray-400 flex flex-row h-11 w-80 mb-3 '>
     <img src="searchicon.png" alt="search" />
    <p>Search by name or email</p>
 </div>
-<div className='bg-[#0B111A] flex flex-row px-4 py-2 text-white gap-8'>
+<div className=' rounded-xl mb-3'>
+  <div className='bg-[#0B111A] flex flex-row justify-between px-4 py-2 text-white  w-[1050px] border border-gray-700'>
    <div>No#</div>
    <div>Image</div>
    <div>Full Name</div>
    <div>Email</div>
    <div>Phone Number</div>
    <div>Action</div>
+</div>
+ <div className='flex flex-col justify-center items-center w-full py-12 gap-8 border border-gray-700'>
+     <img src="Component 5.png" alt="" />
+<div className='gap-2 text-center flex flex-col justify-center items-center'>
+           <h4 className='text-lg text-white font-semibold font-onset'>No Data Available yet</h4>
+    <p className='text-sm text-white'>Add a new customer to see them here</p>
+</div>
+ </div>
+ 
 </div>
       </div>
     </div>
