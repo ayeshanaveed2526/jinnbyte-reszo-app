c3d8cfbbf4f8d302e9ced3f1bb3a6cd66dc3fb55
Ayesha Naveed <hr34772@gmail.com>
2025-07-06 17:42:01 +0500

    deleteicon

--- src/RestaurantMang.jsx
+++ src/RestaurantMang.jsx
@@ -4,6 +4,7 @@ import Sidebar from "./Sidebar";
 import BookingPage from "./BookingPage";
 
 export default function RestaurantMang() {
+  const [deleteId, setDeleteId] = useState(null);
   const [data, setData] = useState(
     Array.from({ length: 50 }, (_, index) => ({
       id: index + 1,
@@ -24,6 +25,7 @@ export default function RestaurantMang() {
 
   const handleDelete = (id) => {
     setData((prev) => prev.filter((item) => item.id !== id));
+    setDeleteId(id);
   };
 
   const allcolumns = [
@@ -68,6 +70,32 @@ export default function RestaurantMang() {
           />
         </div>
       </div>
+      {deleteId && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
+          <div className="bg-[#1B2D47] rounded-lg shadow-lg p-8 w-[440px] h-[285px] gap-[15px] flex flex-col items-center justify-center">
+            <img src="deleteicon.png" alt=""  />
+            <div className="text-white text-lg mb-4 font-semibold font-onset text-center ">Are you sure you want to delete this 
+              <div className="text-gray-300 font-normal mt-2 "> 
+                This action cannot be undone. This will be permanently removed from the database.
+              </div>
+            </div>
+            <div className="flex justify-end gap-4">
+              <button
+                className="px-7 py-2 border border-white text-white rounded-[8px]"
+                onClick={() => setDeleteId(null)}
+              >
+                Cancel
+              </button>
+              <button
+                className=" bg-[#FF5655] text-white hover:bg-red-600 rounded-[8px] px-7 py-2 "
+                onClick={() => handleDelete(deleteId)}
+              >
+                Yes, Delete it
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
